24ff94faf78be62dabfa524615549ff4358e7fc5
Vic Watson <171040641+VJWatson@users.noreply.github.com>
2026-02-13 16:22:08 +0000

    app deps (#55)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "9.18.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "10.5.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-backend-$playVersion"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVersion"           % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% s"internal-auth-client-$playVersion" % "4.0.0",
-    "uk.gov.hmrc"            %% s"crypto-json-$playVersion"          % "8.2.0"
+    "uk.gov.hmrc"            %% s"internal-auth-client-$playVersion" % "4.3.0",
+    "uk.gov.hmrc"            %% s"crypto-json-$playVersion"          % "8.4.0"
 
   )
 
